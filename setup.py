3c185028c6eb1100bfd5e7931d0553d805a851e7
Patrik Dufresne <info@patrikdufresne.com>
2019-08-06 15:01:56 -0500

    WIP

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Coin-or CBC native interface for Python
-# Copyright (C) 2019 Patrik Dufresne Service Logiciel inc.
-#
-# TODO Add the right license.
+# Coin-or CBC native interface for Python
+# Copyright (C) 2019 Patrik Dufresne Service Logiciel <info@patrikdufresne.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 try:
     from setuptools import setup, Extension
@@ -19,8 +32,12 @@ setup(
     author='Patrik Dufresne',
     author_email='info@patrikdufresne.com',
     url='https://git.patrikdufresne.com/pdsl/cbcpy',
-    license="GPLv3",
-    ext_modules=[Extension('_cbcpy', ['cbcpy.i'], swig_opts=['-modern', '-I../include'])],
+    license="",
+    ext_modules=[Extension(
+        '_cbcpy',
+        ['cbcpy.i'],
+        swig_opts=['-modern', '-I../include'],
+        libraries=['CbcSolver','Cbc','Cgl','OsiClp','OsiCbc','Osi','Clp', 'CoinUtils', 'bz2', 'z'])],
     py_modules=['cbcpy'],
     setup_requires=[
         "setuptools_scm",
