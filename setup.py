5c3caf5c56dd6ced9a41366101d1465d07127689
Patrik Dufresne <info@patrikdufresne.com>
2019-08-14 09:54:16 -0400

    Patch using python

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Coin-or CBC native interface for Python
-# Coin-or CBC native interface for Python
 # Copyright (C) 2019 Patrik Dufresne Service Logiciel <info@patrikdufresne.com>
 # 
 # This program is free software; you can redistribute it and/or
@@ -20,10 +19,12 @@
 import os
 try:
     from setuptools import setup, Extension
+    from setuptools.command.build_ext import build_ext as _build_ext
 except ImportError:
     import ez_setup
     ez_setup.use_setuptools()
     from setuptools import setup, Extension
+    from setuptools.command.build_ext import build_ext as _build_ext
 
 # Configure the SWIG build
 CBC_DIR = os.environ.get('CBC_DIR', './Cbc')
@@ -39,12 +40,30 @@ if os.name == 'nt':
 else:
     extra_objects = ['%s/lib%s.a' % (library_dirs[0], l) for l in libraries]
     libraries = []
-swig_opts=['-c++','-doxygen']
+swig_opts = ['-c++', '-doxygen']
 swig_opts.extend(['-I%s' % i for i in include_dirs])
 
-# Define project description
-project_dir = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(project_dir, 'README.md')) as f:
+
+# Extend build_ext to patch the include files.
+class cbc_build_ext(_build_ext):
+    
+    def _patch_headers(self):
+        import patch
+        patches_dir = os.path.join(os.path.dirname(__file__), 'patches')
+        for f in os.listdir(patches_dir):
+            f = os.path.join(patches_dir, f)
+            pset = patch.fromfile(f)
+            if not pset.apply(strip=2, root=CBC_DIR):
+                print('fail to patch file: ' + f)
+    
+    def run(self):
+        self._patch_headers()
+        _build_ext.run(self)
+
+# Define project description from README.md
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
     long_description = f.read()
 
 setup(
@@ -56,7 +75,8 @@ setup(
     author='Patrik Dufresne',
     author_email='info@patrikdufresne.com',
     url='https://git.patrikdufresne.com/pdsl/cbcpy',
-    license="",
+    license="LGPL",
+    cmdclass={'build_ext': cbc_build_ext},
     ext_modules=[Extension(
         '_cbcpy',
         ['cbcpy.i'],
@@ -66,4 +86,5 @@ setup(
         libraries=libraries,
         extra_objects=extra_objects)],
     py_modules=['cbcpy'],
+    setup_requires=['patch'],
 )
